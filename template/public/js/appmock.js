970352edfea9fb4fc0935faa3e07fb0ef7ad9aab
cyij <cyij2006@gmail.com>
2017-10-27 17:41:34 +0800

    add public

---
+++ template/public/js/appmock.js
@@ -0,0 +1,17 @@
+var __noop = function () {};
+var AMap = {
+  getLocation: __noop
+}
+
+var CordovaCommon = {
+  login: __noop,
+  device: __noop,
+  tel: __noop,
+  showMap: __noop,
+  hideMap: __noop,
+  driveRoute: __noop
+}
+
+var Pay = {
+  pay: __noop
+}
