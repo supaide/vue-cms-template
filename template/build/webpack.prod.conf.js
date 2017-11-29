aa338cc9052f754c00d8e1bea1067f2241b2c3de
cyij <cyij2006@gmail.com>
2017-11-30 00:23:48 +0800

    up

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -34,7 +34,6 @@ const webpackConfig = merge(baseWebpackConfig, {
       'process.env': env
     }),
     // UglifyJs do not support ES6+, you can also use babel-minify for better treeshaking: https://github.com/babel/minify
-    /*
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
@@ -42,7 +41,6 @@ const webpackConfig = merge(baseWebpackConfig, {
       sourceMap: config.build.productionSourceMap,
       parallel: true
     }),
-    */
     // extract css into its own file
     new ExtractTextPlugin({
       filename: utils.assetsPath('css/[name].[contenthash].css'),
